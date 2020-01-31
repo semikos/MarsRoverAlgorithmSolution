79658af98f902cbf3555232c85c167fd6008fae7
semikos <samykobbi@gmail.com>
2020-01-31 02:21:18 +0100

    MarsRover: Add some custom validation using regex

--- src/main/java/com/bnpcid/interview/MarsRover.java
+++ src/main/java/com/bnpcid/interview/MarsRover.java
@@ -21,13 +21,25 @@ public class MarsRover {
 			for (int i = 0; i < gameLines.size(); i++) {
 				String gameLine = gameLines.get(i);
 				if (i == 0) {
+					if (!gameLine.matches("\\d+\\s\\d+")) {
+						System.err.print("Invalid grid dimensions");
+						break;
+					}
 					String[] endGridLine = gameLines.get(0).split(" ");
 					r = new RoverGrid(Integer.parseInt(endGridLine[0]), Integer.parseInt(endGridLine[1]));
 				} else if (i % 2 == 1) {
+					if (!gameLine.matches("\\d+\\s\\d+\\s[NEWS]")) {
+						System.err.print("Invalid initial position");
+						break;
+					}
 					String[] initPosArray = gameLine.split(" ");
 					initPos = new Position(Integer.parseInt(initPosArray[0]), Integer.parseInt(initPosArray[1]),
 							Direction.valueOf(initPosArray[2]));
 				} else {
+					if (!gameLine.matches("[MRL]+")) {
+						System.err.print("Invalid controls");
+						break;
+					}
 					char[] controls = gameLine.toCharArray();
 					finalPos = r.findFinalPosition(initPos, controls);
 					System.out.println(finalPos);
